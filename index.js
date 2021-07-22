d564e2330bbb53f022eb2690b1b24e7cd9933275
OnceRobert <87625361+OnceRobert@users.noreply.github.com>
2021-07-22 10:34:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
-
+name = toUpperCase(name);
+name = " '" + name + "' ";
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+			Temp = sentence.split(' ');
+      sentence = '';
+			for(var i = 0; i<Temp.length;i++){
+      		Cap = Temp[i];
+          Cap = Cap.replace(Cap.charAt(0),(Cap.charAt(0)).toUpperCase())
+		    	sentence = sentence + Cap + ' ';
+		}
+    document.write(sentence);
+
+
+
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+    re = /\D/g;
+  	money = money.replaceAll(re,'');
